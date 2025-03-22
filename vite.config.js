27a3da8ac4b9cea61dbb9bf17f9214964f597e4a
Malitha <141618487+Malitha-Gunathilaka@users.noreply.github.com>
2025-03-22 21:58:13 +0530

    chore: add Vercel configuration and update package dependencies

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,5 @@ export default defineConfig({
   plugins: [react()
   , tailwindcss(),
   ],
-  base: process.env.VITE_BACE_PATH || "/LAVENDRA-TEA",
+  // base: process.env.VITE_BACE_PATH || "/LAVENDRA-TEA",
 })
